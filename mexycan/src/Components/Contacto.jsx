112b849f35998ddd6cc1750defb1a4f84d3a99ce
gabriel <gabrielmedinatorres@hotmail.com>
2024-08-08 18:51:20 -0600

    contacto

--- mexycan/src/Components/Contacto.jsx
+++ mexycan/src/Components/Contacto.jsx
@@ -1,5 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./Contacto.css";
+
 const Contacto = () => {
-  return <div>Contacto</div>;
+  return (
+    <div className="contacto-container">
+      <h1 className="contacto-heading">
+        Para sus pedidos pulse{" "}
+        <a
+          href="https://wa.me/524497781615?text=Hola%20me%20gustar%C3%ADa%20hacer%20un%20pedido"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="contacto-link"
+        >
+          aquí
+        </a>
+      </h1>
+    </div>
+  );
 };
 
 export default Contacto;
