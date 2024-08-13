1898a31ba4796855bdcabe1753d3be4ab1bbe317
gabriel <gabrielmedinatorres@hotmail.com>
2024-08-12 19:49:36 -0600

    contacto prev

--- mexycan/src/Components/Contacto.jsx
+++ mexycan/src/Components/Contacto.jsx
@@ -8,7 +8,7 @@ const Contacto = () => {
       <h1 className="contacto-heading">
         Para sus pedidos pulse{" "}
         <a
-          href="https://wa.me/524497781615?text=Hola%20me%20gustar%C3%ADa%20hacer%20un%20pedido"
+          href="https://wa.me/524497781615?text=Hola%20me%20gustar%C3%ADa%20hacer%20un%20pedido%20de%20comida%20para%20llevar"
           target="_blank"
           rel="noopener noreferrer"
           className="contacto-link"
@@ -16,6 +16,14 @@ const Contacto = () => {
           aquí
         </a>
       </h1>
+      <div className="imagenes">
+        <div className="croqueta"></div>
+        <div className="tortilla"></div>
+        <div className="torta"></div>
+        <div className="tumaca"></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
